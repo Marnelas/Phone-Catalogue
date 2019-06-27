87641dfe5f98dc586647ecc8a51c497ff7927076
GomezOrtiz <david.gomez.mail@gmail.com>
2019-06-27 10:17:11 +0200

    Deploying

---
+++ server/public/precache-manifest.712e64ffcd4a7c4a17382319260dab86.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38c495ec6e2f203a546f0f20c1db5f1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18896c2efb75b009bea2",
+    "url": "/static/js/2.058a628a.chunk.js"
+  },
+  {
+    "revision": "91a1e68924262173550d",
+    "url": "/static/js/main.5668107e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
